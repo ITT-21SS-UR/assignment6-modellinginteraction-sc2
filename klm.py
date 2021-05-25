de5b1a3680d1fb81934520a4d340292fb2819204
Claudia Schoenherr <mai-claudia.schoenherr@stud.uni-regensburg.de>
2021-05-25 20:21:11 +0200

    Fix klm bug with one line

--- klm.py
+++ klm.py
@@ -1,35 +1,54 @@
+import os
 import sys
+
+"""
+Reads a file and outputs two different predictions (standard, custom set)
+for the task completion time of the calculator.
+
+File restrictions:
+    When a comment is written a # is not allowed within the comment.
+    Allowed characters are k, p, b, h, m (case insensitive)
+"""
+
 
 # Author: Sarah
 # Reviewer: Claudia
 
 
 def read_file(filename):
-    file = open(filename, 'r')
-    lines = file.readlines()
+    if len(sys.argv) < 2:
+        sys.stderr.write("Please give a valid klm txt file as argument (-_-)\n")
+        sys.exit(1)
 
-    count = 0
+    if not os.path.isfile(filename):
+        sys.stderr.write("File does not exist (-_-)\n")
+        sys.exit(1)
+
+    file = open(filename, 'r')
     data = []
 
     # Strips at newline character
-    for line in lines:
-        data.append(line.strip())
-        count += 1
+    for line in file.readlines():
+        striped_line = line.strip()
+
+        if striped_line:
+            data.append(striped_line)
 
     return data
 
 
-def delete_comments(data):
+def clean_data(data):
     cleaned_data = []
 
     for i in range(0, len(data)):
-        comment_char_pos = data[i].rfind("#")
-        current_line = data[i][:comment_char_pos].strip()
+        current_line = data[i]
+        comment_char_pos = data[i].rfind("#")  # finds the index of the last #
+
+        if comment_char_pos != -1:
+            current_line = data[i][:comment_char_pos].strip()
 
         if current_line != "":
-            cleaned_data.append(current_line)
-
-        i += 1
+            cleaned_data.append(current_line.lower())
 
     return cleaned_data
 
@@ -40,19 +59,21 @@ def calc_klm_time_one_line(line_data):
     current_number = ""
 
     for i in range(0, len(line_data)):
-        if line_data[i].isdigit():
-            current_number += str(line_data[i])
+        current_line = line_data[i]
+        if current_line.isdigit():
+            current_number += str(current_line)
 
-        if not line_data[i].isdigit():
+        else:
             if current_number == '':
-                result_klm += get_klm_value(line_data[i].lower())
-                result_experiment += get_experiment_value(line_data[i].lower())
+                result_klm += get_klm_value(current_line)
+                result_experiment += get_experiment_value(current_line)
             else:
-                result_klm += int(current_number) * get_klm_value(line_data[i].lower())
-                result_experiment += int(current_number) * get_experiment_value(line_data[i].lower())
+                result_klm += int(current_number) * get_klm_value(current_line)
+                result_experiment += int(current_number) * get_experiment_value(current_line)
+
             current_number = ""
 
-    return (result_klm, result_experiment)
+    return result_klm, result_experiment
 
 
 def get_klm_value(char):
@@ -67,7 +88,7 @@ def get_klm_value(char):
     if char == 'm':
         return 1.2
 
-    return
+    return -1
 
 
 def get_experiment_value(char):
@@ -82,7 +103,7 @@ def get_experiment_value(char):
     if char == 'm':
         return 1.2  # standard value because not calculated during study
 
-    return
+    return -1
 
 
 def calc_total_klm_time(data):
@@ -90,21 +111,20 @@ def calc_total_klm_time(data):
     result_experiment = 0.0
 
     for i in range(0, len(data)):
-        result_klm += calc_klm_time_one_line(data[i].lower())[0]
-        result_experiment += calc_klm_time_one_line(data[i].lower())[1]
+        result_klm += calc_klm_time_one_line(data[i])[0]
+        result_experiment += calc_klm_time_one_line(data[i])[1]
 
     print("Time for standard klm: {}".format(result_klm))
     print("Time for experiment klm: {}".format(result_experiment))
 
-    return (result_klm, result_experiment)
+    return result_klm, result_experiment
+
+
+def calculate_klm_estimates():
+    file_data = read_file(sys.argv[1])
+    processed_data = clean_data(file_data)
+    calc_total_klm_time(processed_data)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        sys.stderr.write("Please give a valid klm txt file as argument (-_-)\n")
-        sys.exit(1)
-
-    file_data = read_file(sys.argv[1])
-    new_data = delete_comments(file_data)
-
-    calc_total_klm_time(new_data)
+    calculate_klm_estimates()
